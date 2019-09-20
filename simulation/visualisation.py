65d217397d976145e27c3d522550117c13198797
Titus Senez <titus.senez@polytechnique.edu>
2019-09-20 13:21:28 +0200

    Add satellite axis and put color

--- simulation/visualisation.py
+++ simulation/visualisation.py
@@ -8,13 +8,20 @@ uy = vp.vector(0,1,0)
 uz = vp.vector(0,0,1)
 
 # trièdre (z,x,y)
-axe_x = vp.arrow(pos=vp.vector(0,0,0), axis=10*ux, shaftwidth=0.1)
-axe_y = vp.arrow(pos=vp.vector(0,0,0), axis=10*uy, shaftwidth=0.1)
-axe_z = vp.arrow(pos=vp.vector(0,0,0), axis=10*uz, shaftwidth=0.1)
+axe_x_r = vp.arrow(pos=vp.vector(0,0,0), axis=10*ux, shaftwidth=0.1, color=vp.vector(1,0,0))
+axe_y_r = vp.arrow(pos=vp.vector(0,0,0), axis=10*uy, shaftwidth=0.1)
+axe_z_r = vp.arrow(pos=vp.vector(0,0,0), axis=10*uz, shaftwidth=0.1)
+
+
+axe_x_s = vp.arrow(pos=vp.vector(10,10,10), axis=10*ux, shaftwidth=0.1, color=vp.vector(1,0,0))
+axe_y_s = vp.arrow(pos=vp.vector(10,10,10), axis=10*uy, shaftwidth=0.1)
+axe_z_s = vp.arrow(pos=vp.vector(10,10,10), axis=10*uz, shaftwidth=0.1)
+sugarbox = vp.box(pos=vp.vector(10,10,10), size=vp.vector(5,5,5), axis=vp.vector(0,0,0), up = uy)
+
+satellite = vp.compound([axe_x_s,axe_y_s,axe_z_s,sugarbox])
 
 wx, wy, wz = (1,1,1)
 dt = 1/50
-sugarbox = vp.box(pos=vp.vector(10,10,10), size=vp.vector(5,5,5), axis=vp.vector(0,0,0), up = uy)
 
 L0 = np.array([[0.],[0.],[0.]]) # Moment cinétique initial
 I = np.eye(3) # Tenseur inertie du satellite
@@ -28,6 +35,6 @@ sim = Simulator(dt,L0) #on créée un objet sim qui fera les simus
 while True:
     W = sim.getNextIteration(M,dw,J,B,I) # on récupère le prochain vecteur rotation
     # Rotate: rotation de tout l'objet autour de la droite de vecteur directeur <axis> et passant par <origin>)
-    sugarbox.rotate(angle=np.linalg.norm(W)*dt, axis=vp.vector(W[0][0],W[1][0],W[2][0]))
+    satellite.rotate(angle=np.linalg.norm(W)*dt, axis=vp.vector(W[0][0],W[1][0],W[2][0]), origin=vp.vector(10,10,10))
     # Rate : réalise 50 fois la boucle par seconde
     vp.rate(1/dt)
