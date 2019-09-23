3f337d24f3d5e5608da725832effa196552a5e68
Adrien Garinet <adrien.garinet@polytechnique.edu>
2019-09-23 17:00:11 +0200

    Vitesse initiale random et affichage des composantes.

--- simulation/visualisation.py
+++ simulation/visualisation.py
@@ -3,13 +3,13 @@ import vpython as vp
 from math import *
 import numpy as np
 from scao import SCAO
-
+from random import *
 
 lx,ly,lz = 10,10,10
 m = 1
 dt = 1/50
 I = np.diag((m*(ly**2+lz**2)/3,m*(lx**2+lz**2)/3,m*(lx**2+ly**2)/3)) # Tenseur inertie du satellite
-W0 = np.array([[2],[2],[2]]) #rotation initiale dans le référentiel R_r
+W0 = np.array([[2*random()],[2*random()],[2*random()]]) #rotation initiale dans le référentiel R_r
 L0 = np.dot(I,W0) # Moment cinétique initial !! Attention à bien vérifier que tout est dans le bon référentiel !!
 dw = np.array([[0.],[0.],[0.]]) # vecteur de l'accélération angulaire des RI
 M = np.array([[0.],[0.],[0.]]) # vecteur du moment magnétique des bobines
@@ -36,7 +36,7 @@ satellite = vp.compound([axe_x_s,axe_y_s,axe_z_s,sugarbox])
 b_vector = vp.arrow(pos=vp.vector(-5,-5,-5), axis=10*vp.vector(B[0][0],B[1][0],B[2][0]), shaftwidth=0.1, color=vp.vector(1,1,1))
 
 sim = Simulator(dt,L0) #on créée un objet sim qui fera les simus
-stab = SCAO(I,J,4000,10)
+stab = SCAO(I,J,500,10)
 
 while True:
 
@@ -45,7 +45,7 @@ while True:
     # Sauvegarder les valeurs de simulation actuelles:
     stab.setAttitude(sim.Q)
     stab.setRotation(np.dot(P_v_r,W))
-    print(np.linalg.norm(W))
+    print("W : {},{},{}".format(W[0][0],W[1][0],W[2][0]) +" ; " + str(np.linalg.norm(W)))
     stab.setMagneticField(np.dot(P_v_r,B))
 
     # Calculer le moment magnétique à fournir:
