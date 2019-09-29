8e8ca3bba781d835b583386beaacba0a122b6c4d
Adrien Garinet <adrien.garinet@polytechnique.edu>
2019-09-29 11:15:11 +0200

    Commentaires.

--- scao/scao.py
+++ scao/scao.py
@@ -6,12 +6,13 @@ class SCAO:
 
     def __init__(self, I, J, P_DetumblingMagnetic, P_DetumblingWheel, P_StabMagnetic, P_StabWheel, D_StabMagnetic, D_StabWheel, delta_t):
         """
-        listQ: liste des attitudes
-        listL: liste des moments cinétiques
-        I: matrice d'inertie
-        J: moment d'inertie des roues
+        listQ: liste des attitudes dans Rr
+        listL: liste des moments cinétiques dans Rr
+        listB: liste des champs magnétiques calculées dans Rv
+        Mrv : matrice de transfert de r -> v
+        Mvr : inverse de Mvr
         P_d_mt: facteur P du PID pour les magnetorquers
-        P_d_iw: facteur P du PID pour les roues à inertie
+        P_d_rw: facteur P du PID pour les roues à inertie
         """
         self.Q = [] #exprimé dans Rr
         self.W = [] #exprimé dans Rr
@@ -25,44 +26,49 @@ class SCAO:
         self.P_s_rw = P_StabWheel
         self.D_s_rw = D_StabWheel
 
-    def transferMatrix(self,Q): #transfer matrix from Rr to Rv i.e. X_Rr = M * X_Rv
+    def transferMatrix(self,Q): #
+        """ Calculate transfer matrix from Rr to Rv, i.e. X_Rr = M * X_Rv """
         q0,q1,q2,q3 = Q[0][0],Q[1][0],Q[2][0],Q[3][0]
         return np.array([[2*(q0**2+q1**2)-1, 2*(q1*q2-q0*q3)  , 2*(q1*q3+q0*q2)  ],
                          [2*(q1*q2+q0*q3)  , 2*(q0**2+q2**2)-1, 2*(q2*q3-q0*q1)  ],
                          [2*(q1*q3-q0*q2)  , 2*(q2*q3+q0*q1)  , 2*(q0**2+q3**2)-1]])
 
-    def setAttitude(self, Q): #Ajoute le quaternion actuel à la liste des attitudes.
+    def setAttitude(self, Q):
+        """ Ajoute le quaternion actuel à la liste des attitudes."""
         self.Q.append(Q)
         self.Mrv = self.transferMatrix(Q) #X_Rr = Mrv * X_Rv
         self.Mvr = np.linalg.inv(self.Mrv) #X_Rv = Mvr * X_Rr
 
     def setRotation(self, W):
-        #le vecteur rotation est mesuré dans R_v
+        """ Ajoute le vecteur rotation actuel à la liste des rotations."""
         self.W.append(W)
 
     def setMagneticField(self, B):
-        #le champ est mesuré dans R_v
+        """ Calcule B dans Rv et l'ajoute."""
         self.B.append(np.dot(self.Mvr,B))
 
-    def getCommandDetumblingMagnetic(self): #Détumbling si accès à la donnée W
-        # En supposant que l'on a accès à W via les instruments de mesure
+    def getCommandDetumblingMagnetic(self):
+        """ Détumbling magnétique si accès à la donnée W via les instruments de mesure."""
         return self.P_d_mt*np.cross(np.dot(self.Mvr,self.W[-1]), self.B[-1], axisa=0, axisb=0,axisc=0)
 
-    def getCommandDetumblingMagneticBDot(self): #détumbing si on connait uniquement le champ B instantané
-        #On utilise le fait que WxB = dB/dt (B-dot algorithm)
+    def getCommandDetumblingMagneticBDot(self):
+        """ Détumbing magnétique si on connait uniquement le champ B instantanné.
+            On utilise le fait que WxB = dB/dt (B-dot algorithm)"""
         dB = (self.B[-1]-self.B[-2])/self.delta_t
         return -self.P_d_mt*dB
 
     def getCommandDetumblingWheel(self):
+        """ Détumbling mécanique si accès à W."""
         return self.P_d_rw*(np.dot(self.Mvr,self.W))
 
     def getCommandStabWheel(self,Qt):
+        #Qt = targeted attitude
         #proportional term
         Qr = Quaternion(*Qt[:,0])*Quaternion(*self.Q[-1][:,0]).inv() #quaternion relatif qui effectue la rotation depuis Q vers Qt
         Qr = Qr.vec()
         r = np.array([Qr[1:,0]])
         r = np.transpose(r/np.linalg.norm(r)) #r est le vecteur autour duquel il faut tourner : axe du couple à appliquer!
-        e = acos(max(-1,min(Qr[0,0],1)))*2 #angle de rotation à effecter : correspond à l'erreur!
+        e = acos(max(-1,min(Qr[0,0],1)))*2 #angle de rotation à effectuer : correspond à l'erreur!
         a = np.dot(self.Mvr,np.dot(self.P_s_rw, -e*r))
 
         #derivative term
