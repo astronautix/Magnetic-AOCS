725707b6cac78aeea092fd4e53ba8b785f273251
Titus Senez <titus.senez@polytechnique.edu>
2019-12-20 14:36:47 +0100

    :art: create a nice detumbling file

--- simulation/detumbling.py
+++ simulation/detumbling.py
@@ -1,10 +1,9 @@
 import sys
 sys.path.append('..')
-from simulator import Simulator
-from plotters import plotAttitude
 import vpython as vp
 from math import *
 import numpy as np
+from simulator import Simulator
 from scao.scao import SCAO
 from scao.stabAlgs import PIDRW, PIDMT
 from environnement.environment import Environment
@@ -13,6 +12,9 @@ from hardware.hardware import Hardware
 from random import *
 import matplotlib.pyplot as plt
 
+####################
+# Fonctions utiles #
+####################
 def rd():
     return 2*(random()-0.5)
 
@@ -21,15 +23,42 @@ def plotAttitude(qs,dt):
         plt.plot([dt*i for i in range(len(qs))],[q.vec()[i,0] for q in qs])
     plt.show()
 
-# Paramètres de la simulation
+###############################
+# Paramètres de la simulation #
+###############################
+# Temps
 dt = 100000000 #pas de temps de la simulation
 fAffichage = 25 #fréquence d'affichage
+
+# Géométrie
 lx,ly,lz = 10,10,10 #longueur du satellit selon les axes x,y,z
 m = 1 #masse du satellite
-J = 1 # moment d'inertie des RW
+
+# Mouvement
 W0 = 0*np.array([[rd()],[rd()],[rd()]]) #rotation initiale dans le référentiel R_r
 Qt = np.array([[0.5],[0.5],[0.5],[0.5]]) #quaternion objectif
 
+# Hardware
+n_windings = 400
+A_coils = 8.64e-3
+M_max = 0.13 #Moment maximum des MT
+J = 1 # moment d'inertie des RW
+
+# Orbite
+omega = 0
+i = pi/6
+e = 0.01
+r_p = 7e6
+mu = 100
+tau = 0
+
+# Environment
+B_model = 'wmm'
+
+
+###################################
+# Initialisation de la simulation #
+###################################
 # Initialisation des variables de simulation
 t = 0
 nbit = 0
@@ -40,10 +69,12 @@ L0 = np.dot(I,W0)
 Wr = []
 qs = []
 
-# Environnement :
-orbite = Orbit(0, pi/4, 0, 7e6, 100)
-environnement = Environment('wmm')
-hardW = Hardware(400,8.64e-3,0.13) #n (number), A (m^2), M_max (A.m^2)
+# Environnement
+orbite = Orbit(omega, i, e, r_p, mu, tau)
+environnement = Environment(B_model)
+
+# Hardware
+hardW = Hardware(n_windings, A_coils, M_max)
 
 # Initialisation du champ magnétique:
 orbite.setTime(t)
@@ -56,7 +87,9 @@ sim = Simulator(dt,L0)
 # Algortihmes de stabilisation
 stab = SCAO(PIDRW(3,3,2),PIDMT(5e-11,50,100),0,I,J,dt) #stabilisateur
 
-## Initialisation graphique ##
+############################
+# Initialisation graphique #
+############################
 ux = vp.vector(1,0,0)
 uy = vp.vector(0,1,0)
 uz = vp.vector(0,0,1)
@@ -73,6 +106,9 @@ satellite = vp.compound([axe_x_s,axe_y_s,axe_z_s,sugarbox])
 #vecteur champ B
 b_vector = vp.arrow(pos=vp.vector(-5,-5,-5), axis=10*vp.vector(B[0][0],B[1][0],B[2][0]), shaftwidth=0.1, color=vp.vector(1,1,1))
 
+#####################
+# Boucle principale #
+#####################
 while True:
     #on récupère la valeur actuelle du champ magnétique et on actualise l'affichage du champ B
     orbite.setTime(t) #orbite.setTime(t)
