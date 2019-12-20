499e6aefc748733fe29068b3018190430deca092
Titus Senez <titus.senez@polytechnique.edu>
2019-12-20 19:27:52 +0100

    unify stabilization algorithm

--- scao/stabAlgs.py
+++ scao/stabAlgs.py
@@ -2,28 +2,22 @@ import numpy as np
 from scao.quaternion import Quaternion
 from math import acos, cos, pi, sin
 
-def PIDRW(P, D, dP): ## BUG: CHECK HERE FOR REF CONSISTENCY
+def PIDRW(P, dP, D): ## BUG: CHECK HERE FOR REF CONSISTENCY
     def res(Q,W,Qt,B,I):
         #Qt = targeted attitude /!\ N'est pas un objet Quaternion (mais un quaternion formel)
-        #proportional term - the error is expressed in R_v
         Qr = Qt*Q.inv() #quaternion relatif qui effectue la rotation depuis Q vers Qt
         dynamicalP = P/(1+np.linalg.norm(W))**dP #dynamical P-factor
-        error = np.dot(Q.tminv(),dynamicalP*Qr.angle()*Qr.axis())
-
-        #derivative term
-        error += np.dot(Q.tminv(),-D*W)
-
+        u = Q.R2V(dynamicalP*Qr.axialPart() - D*W) #REMPLACER Qr.angle()*Qr.axis() par Qr.axialPart()!!
         #moment à appliquer
-        torque = np.dot(I,error)
-
+        torque = np.dot(I,u)
         return torque
-
     return res
 
-def PIDMT(P, D):
+def PIDMT(P, dP, D):
     def res(Q,W,Qt,B,I):
         Qr = Q*Qt.inv()
-        u = Q.R2V((P*Qr.axialPart()+D*W))
+        dynamicalP = P # P/(1+np.linalg.norm(W))**dP
+        u = Q.R2V((dynamicalP*Qr.axialPart()+D*W))
         Bb = Q.R2V(B)
         M = np.cross(u,Bb,axisa=0,axisb=0,axisc=0)
         return M
