:bug: fix quaternion unproper normalization and incorrect scao for MT

scao/quaternion.py
@@ -1,10 +1,10 @@
 import numpy as np
-from math import acos
+from math import acos, sqrt
 
 class Quaternion:
 
     def __init__(self,a,b,c,d):
-        norm = a**2+b**2+c**2+d**2
+        norm = sqrt(a**2+b**2+c**2+d**2)
         self.a = a/norm
         self.b = b/norm
         self.c = c/norm
@@ -64,6 +64,10 @@ class Quaternion:
 
     def angle(self):
         return acos(max(-1,min(self.a,1)))*2
+
+    def axialPart(self):
+        res = np.array([[self.b],[self.c],[self.d]])
+        return res
 
     def V2R(self,vec):
         return np.dot(self.tm(),vec)

scao/stabAlgs.py
@@ -20,24 +20,11 @@ def PIDRW(P, D, dP): ## BUG: CHECK HERE FOR REF CONSISTENCY
 
     return res
 
-def PIDMT(e, kp, kv):
+def PIDMT(P, D):
     def res(Q,W,Qt,B,I):
         Qr = Q*Qt.inv()
-        u = Q.R2V((e**2*kp*Qr.axis()+e*kv*W))
+        u = Q.R2V((P*Qr.axialPart()+D*W))
         Bb = Q.R2V(B)
         M = np.cross(u,Bb,axisa=0,axisb=0,axisc=0)
         return M
-    return res
-
-def PIDMT2(e, kp, kv):
-    def res(Q,W,Qt,B,I):
-        Bdir = B/np.linalg.norm(B)
-        med = Bdir + np.array([[1],[0],[0]])
-        med = sin(pi/2)*med/np.linalg.norm(med)
-        Q_tb = Quaternion(cos(pi/2), med[0][0], med[1][0], med[2][0])
-        Qr = Qt*Q.inv()
-        rotvec = Q_tb.R2V(Qr.axis())
-        T_b = -(e**2*kp*rotvec+e*kv*np.dot(I,W))
-        T_v = Q.R2V(Q_tb.V2R(T_b))
-        return -T_v
     return res