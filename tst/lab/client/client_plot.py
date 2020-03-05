f2d6645a1122d806b9542bc0c4a1e515c81f1a54
Adrien Garinet <adrien.garinet@polytechnique.edu>
2020-03-05 18:36:15 +0100

    clinet_plot clean

--- tst/lab/client/client_plot.py
+++ tst/lab/client/client_plot.py
@@ -1,17 +1,18 @@
 import sys
-import os
-sys.path.append(os.path.join(*['..']*3))
+sys.path.append('..\..\..')
 import time
 from src.scao.quaternion import Quaternion
 import numpy as np
 from numpy import array
 import requests
 import vpython as vp
+import matplotlib
+matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 lx,ly,lz = 10,10,10 #longueur du satellit selon les axes x,y,z
-dt = 0.01
+dt = 0.1
 
 ####################################
 # Initialisation graphique vpython #
@@ -34,24 +35,26 @@ b_vector = vp.arrow(pos=vp.vector(-5,-5,-5), axis=10*vp.vector(0,0,0), shaftwidt
 
 
 Ws = []
+Wnorms = []
 Qs = []
-t, ts, t_ = [], [], 0
+t, ts, nbr_secondes = [], [], 0
+Wmax = 0
 
 #######################################
 # Initialisation graphique matplotlib #
 #######################################
-
 fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
 
 
 def animate(i, Qs, ts):
-    global t_, dt
-    t_ += dt
+    global nbr_secondes, dt, Wmax
+    # required for dezooming
+    nbr_secondes += dt
     try:
         # Read attitude
         response = requests.get('http://192.168.8.1:5000', verify=False, timeout=0.5)
-        M, W, B, Q = response.text.split("<br/>")
+        temps, M, W, B, Q = response.text.split("<br/><br/>")[-1].split("<br/>")
+        temps = eval(temps)
         Q = Quaternion(*eval(Q)[:,0])
         M = eval(M)
         W = eval(W)
@@ -59,22 +62,48 @@ def animate(i, Qs, ts):
 
         # add Q and t_ to lists
         Ws.append(W)
-        Qs.append(Q)
+        Wnorms.append(np.linalg.norm(W))
+        Qs.append(Q.vec())
         t.append(time.time())
-        ts.append(t_)
+        ts.append(temps)
 
-        # Draw x and y lists
-        ax.clear()
-        for i in range(4):
-            ax.plot(ts, np.array(Qs)[:,i])
+        #update dynamic Wmax
+        Wmax = max(Wmax, Wnorms[-1])
 
-        # Format plot
+
+        plt.clf()
+
+        # Draw quaternion
+        plt.subplot(2,1,1)
+            # vecteur (nuances de bleu)
+        plt.plot(ts, np.array(Qs)[:,1][:,0], label = "$Q_{}$".format(1), c = "#87CEFA")
+        plt.plot(ts, np.array(Qs)[:,2][:,0], label = "$Q_{}$".format(2), c = "#87CEEB")
+        plt.plot(ts, np.array(Qs)[:,3][:,0], label = "$Q_{}$".format(3), c = "#00BFFF")
+            # angle
+        plt.plot(ts, np.array(Qs)[:,0][:,0], label = "$Q_{}$".format(0), c = 'r')
+
+            # Format plot
         plt.xticks(rotation=45, ha='right')
-        plt.subplots_adjust(bottom=0.30)
-        plt.title('Attitude quaternion over Time')
+        plt.title('Attitude quaternion and angular speed over time')
         plt.ylabel('Attitude')
+        plt.legend()
         plt.ylim((-1,1))
-        plt.xlim((0,300))
+        plt.xlim(right = max(30, ts[-1])+int(nbr_secondes))
+
+        # Draw W
+        plt.subplot(2,1,2)
+
+        plt.plot(ts, np.array(Ws)[:,0][:,0], label = "$W_{}$".format(0), c = "#87CEFA")
+        plt.plot(ts, np.array(Ws)[:,1][:,0], label = "$W_{}$".format(1), c = "#87CEEB")
+        plt.plot(ts, np.array(Ws)[:,2][:,0], label = "$W_{}$".format(2), c = "#00BFFF")
+
+        plt.plot(ts, np.array(Wnorms), label = "$|W|$", c = 'r')
+            # Format plot
+        plt.xticks(rotation=45, ha='right')
+        plt.ylabel('Speed')
+        plt.legend()
+        plt.ylim((-Wmax*1.05,Wmax*1.05))
+        plt.xlim(right = max(30, ts[-1])+int(nbr_secondes))
 
         # Actualisation de l'affichage graphique
         b_vector.axis = 10*vp.vector(*B[:,0]/np.linalg.norm(B))
@@ -82,10 +111,10 @@ def animate(i, Qs, ts):
         satellite.up = vp.vector(*Q.V2R(array([[0],[0],[1]]))[:,0])
 
     except requests.exceptions.ConnectionError:
-        print('[' + str(int(time.time())) + "] Did not reached server")
+        print('[' + str(int(time.time())) + "] Did not reach server")
     except requests.exceptions.ReadTimeout:
-        print('[' + str(int(time.time())) + "] Did not reached server")
+        print('[' + str(int(time.time())) + "] Did not reach server")
 
 # Set up plot to call animate() function periodically
-ani = animation.FuncAnimation(fig, animate, fargs=(ts, Qs), interval=100)
+ani = animation.FuncAnimation(fig, animate, fargs=(ts, Qs), interval=1/dt)
 plt.show()
