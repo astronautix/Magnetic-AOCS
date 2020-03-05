108891d665106f1daa99d5f13dae8119b365c03b
Titus Senez <titus.senez@polytechnique.edu>
2020-03-05 18:40:37 +0100

    fix append de bébé

--- tst/lab/helmoltz/main.py
+++ tst/lab/helmoltz/main.py
@@ -2,8 +2,8 @@ from helmoltz import *
 
 #on crée les objets représentant chaque générateurs en indiquant leur ports USB
 #sur linux on les découvre avec la commande 'ls /dev/ttyUSB*'
-gen1 = GPD3303S("/dev/ttyUSB1")
-gen2 = GPD3303S("/dev/ttyUSB0")
+gen1 = GPD3303S("/dev/ttyUSB0")
+gen2 = GPD3303S("/dev/ttyUSB1")
 
 #on définit la tension des générateurs suffisemment haut pour ne pas être régulé en tension
 gen1.V_ch1 = Q_("30V")
